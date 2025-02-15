dbf36733e81409f928fcf128d2a292abd1f0de07
Philippe.martens <philippe.martens@cgi.com>
2025-02-15 21:40:07 +0100

    Updated docker install script

--- script/docker-install.sh
+++ script/docker-install.sh
@@ -1,6 +1,6 @@
 echo "Updating and installing required packages"
-sudo apt update 2> /dev/null
-sudo apt install apt-transport-https 2> /dev/null
+sudo apt-get update > /dev/null
+sudo apt-get install apt-transport-https > /dev/null
 
 echo "Adding docker gpg to keyring" 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker.gpg
@@ -9,8 +9,8 @@ echo "Adding docker repository"
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Installing docker"
-sudo apt update 2> /dev/null
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 2> /dev/null
+sudo apt-get update > /dev/null
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
 
 echo "Adding ${USER} to docker group"
 sudo usermod -aG docker ${USER}
