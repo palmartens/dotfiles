2079960a6ccf05e0a9b1e771d99ee0cf1254603f
Philippe.martens <philippe.martens@cgi.com>
2025-02-19 11:14:31 +0100

    added ubuntu preferences to bootstrap.sh

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -6,8 +6,20 @@ echo "$DISTRO_NAME Detected"
 
 if [ "$DISTRO_NAME" == "Ubuntu" ] 
 then
-  echo "Ubuntu settings"
-  
+  echo "Setting Ubuntu preferences"
+  echo 'Enable dark mode'
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+	  gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-olive-dark'
+    echo "Disable blank screen delay"
+    gsettings set org.gnome.desktop.session idle-delay 0
+    echo "Disable automatic screen lock"
+    gsettings set org.gnome.desktop.screensaver lock-enabled false
+    echo "Setup the dock"
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false    
+    gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+    gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 40
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true  
 else
   echo "Setting Mint (Cinnamon) preferences"
   gsettings set org.nemo.preferences desktop-is-home-dir false
@@ -21,9 +33,7 @@ else
   gsettings set org.gnome.desktop.interface icon-theme 'Mint-Y'
   gsettings set org.x.apps.portal color-scheme 'prefer-dark'
   gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-ac 0
-  gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-battery 0
-  
-
+  gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-battery 0  
 fi
 
 
