eadc572a7350029ee4552cf84ac6fa844bb55f75
Philippe.martens <philippe.martens@cgi.com>
2025-02-21 00:25:02 +0100

    added fd install, updated ripgrep

--- script/ripgrep-install.sh
+++ script/ripgrep-install.sh
@@ -1,6 +1,7 @@
 . ./functions.sh
 isInstalled rg
 
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb
-sudo dpkg -i ripgrep_14.1.0-1_amd64.deb > /dev/null
-rm ripgrep_14.1.0-1_amd64.deb
+wget --progress=bar:force:noscroll --show-progress -cq "https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb" -O /tmp/ripgrep.deb 2>&1
+
+sudo dpkg -i /tmp/ripgrep.deb > /dev/null
+rm /tmp/ripgrep.deb
