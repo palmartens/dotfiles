18a1c561667bcda0517841caa2153494543dd57c
Philippe.martens <philippe.martens@cgi.com>
2025-04-29 13:46:53 +0200

    added info about logout to enable extensions

--- script/gnome-extensions.sh
+++ script/gnome-extensions.sh
@@ -2,3 +2,4 @@ echo "Installing Gnome extension manager and extensions"
 yay -S extension-manager gnome-shell-extension-dash-to-dock --noconfirm --needed
 echo "Enable Dash-To-Dock"
 gnome-extensions enable dash-to-dock@micxgx.gmail.com
+echo "Extensions installed, logout and login again to see changes"
